9794adbeb1986c7d2635c88e52bbbcf329a95ebc
lang tu van <langtuvan@gmail.com>
2026-01-31 11:03:42 +0700

    g2

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'qsmm-backend',
-      script: './dist/main.js',
+      script: './dist/src/main.js',
       instances: 1,
       exec_mode: 'cluster',
       autorestart: true,
