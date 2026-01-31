2a7b2a7cdbecdc8f3e5a50efe3b370dbc6a1e790
lang tu van <langtuvan@gmail.com>
2026-01-31 11:30:36 +0700

    g2

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
       max_memory_restart: '1G',
       env_production: {
         NODE_ENV: 'production',
-        //APP_PORT:  5001,
+        APP_PORT:  5001,
       },
       error_file: './logs/err.log',
       out_file: './logs/out.log',
